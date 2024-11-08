e07e68bd9ad67af080bb6a8d571014eab1ae8647
DijanckoCalitz <dijancko@gmail.com>
2024-11-08 11:16:49 +0200

    added student class

--- Gui.cs
+++ Gui.cs
@@ -17,112 +17,16 @@ namespace project282
             InitializeComponent();
         }
 
-        static FileManager dataManager = new FileManager();
-
-        static List<Students> students = new List<Students>();
-
         private void Gui_Load(object sender, EventArgs e)
         {
-            students = dataManager.ReadFile("students.txt");
-            mainDataGridView.DataSource = dataManager.ReadFile("students.txt");
         }
 
         private void addStudentBtn_Click(object sender, EventArgs e)
         {
-            if (studentID.Text == "")
-            {
-                errorMessage.Text = $"Please fill in all feilds";
-                return;
-            }
-            else if (studentName.Text == "")
-            {
-                errorMessage.Text = $"Please fill in all feilds";
-                return;
-            }
-            else if (studentAge.Text == "")
-            {
-                errorMessage.Text = $"Please fill in all feilds";
-                return;
-            }
-            else if (studentCourse.Text == "")
-            {
-                errorMessage.Text = $"Please fill in all feilds";
-                return;
-            }
-            else
-            {
-                errorMessage.Text = $"";
-            }
-
-            if (!studentAge.Text.All(char.IsDigit))
-            {
-                errorMessage.Text = $"Age must be a number";
-                return;
-            }
-
-            students = dataManager.ReadFile("students.txt");
-            foreach (Students student in students)
-            {
-                if (student.Id == studentID.Text)
-                {
-                    errorMessage.Text = $"Student of ID {studentID.Text} already exists";
-                    return;
-                }
-            }
-
-            students.Add(new Students(studentID.Text, studentName.Text, int.Parse(studentAge.Text), studentCourse.Text));
-
-            dataManager.WriteFile("students.txt", students);
-
-            mainDataGridView.DataSource = dataManager.ReadFile("students.txt");
         }
 
         private void updateStudent_Click(object sender, EventArgs e)
         {
-            students = dataManager.ReadFile("students.txt");
-
-            if (studentID.Text == "")
-            {
-                errorMessage.Text = $"Please enter student ID";
-                return;
-            }
-
-            foreach (Students student in students)
-            {
-                if (student.Id == studentID.Text)
-                {
-                    if (studentName.Text == "")
-                    {
-                        studentName.Text = student.Name;
-                    }
-                    if (studentAge.Text == "")
-                    {
-                        studentAge.Text = student.Age.ToString();
-                    }
-                    if (studentCourse.Text == "")
-                    {
-                        studentCourse.Text = student.Course;
-                    }
-
-                    student.Id = studentID.Text;
-                    student.Name = studentName.Text;
-                    student.Age = int.Parse(studentAge.Text);
-                    student.Course = studentCourse.Text;
-                    break;
-                }
-                else
-                {
-                    errorMessage.Text = $"Student ID {studentID.Text} not found";
-                }
-            }
-
-            if (students.Count == 0)
-            {
-                errorMessage.Text = $"No students in file to update";
-            }
-
-            dataManager.WriteFile("students.txt", students);
-            mainDataGridView.DataSource = dataManager.ReadFile("students.txt");
         }
 
         private void deleteStudent_Click(object sender, EventArgs e)
